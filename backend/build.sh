d958f48a07845f1fd8740cf5a15bfb69f582088c
Ker Ruben Ramos <xdiscent@gmail.com>
2026-06-05 05:19:56 +0800

    Update build script to automatically seed admin user and data on deployment

--- backend/build.sh
+++ backend/build.sh
@@ -10,3 +10,9 @@ python manage.py collectstatic --no-input
 
 # Run migrations
 python manage.py migrate
+
+# Seed admin user (only creates if doesn't exist)
+python manage.py seed_admin
+
+# Seed academic data (only creates if doesn't exist)
+python manage.py seed_academic_data
