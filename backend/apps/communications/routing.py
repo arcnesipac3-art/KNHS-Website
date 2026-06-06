aa75457f661bb67c970b41b42352ab19d963c69f
Ker Ruben Ramos <xdiscent@gmail.com>
2026-06-06 22:55:38 +0800

    Try multiple WebSocket path patterns

--- backend/apps/communications/routing.py
+++ backend/apps/communications/routing.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from .consumers import MessagesConsumer
 
 
 websocket_urlpatterns = [
     path("ws/messages/", MessagesConsumer.as_asgi()),
+    path("ws/messages", MessagesConsumer.as_asgi()),
+    re_path(r"ws/messages/?", MessagesConsumer.as_asgi()),
 ]
