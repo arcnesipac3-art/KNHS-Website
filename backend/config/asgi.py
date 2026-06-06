2f9758b2d31104c59a3b3ba22ecf2b91ec2327d1
Ker Ruben Ramos <xdiscent@gmail.com>
2026-06-07 00:29:00 +0800

    Defer Django model imports to fix AppRegistryNotReady error

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -5,16 +5,17 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-from apps.communications.routing import websocket_urlpatterns
-from config.websocket_auth import JWTAuthMiddlewareStack
-
 django_asgi_app = get_asgi_application()
+
+# Defer imports that require Django apps to be ready
+def get_websocket_application():
+    from apps.communications.routing import websocket_urlpatterns
+    from config.websocket_auth import JWTAuthMiddlewareStack
+    return JWTAuthMiddlewareStack(URLRouter(websocket_urlpatterns))
 
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        "websocket": JWTAuthMiddlewareStack(
-            URLRouter(websocket_urlpatterns)
-        ),
+        "websocket": get_websocket_application(),
     }
 )
