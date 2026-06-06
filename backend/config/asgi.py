bc1a54784eae92ed930bb1d3baad3f2b1c558cc9
Ker Ruben Ramos <xdiscent@gmail.com>
2026-06-07 00:23:15 +0800

    Fix Django settings configuration for ASGI deployment

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -1,12 +1,12 @@
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
 from apps.communications.routing import websocket_urlpatterns
 from config.websocket_auth import JWTAuthMiddlewareStack
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 django_asgi_app = get_asgi_application()
 
